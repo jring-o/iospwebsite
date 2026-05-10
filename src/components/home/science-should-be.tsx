e00bdfc013955c5b11d9b42d72265e5ab4972fb4
jring-o <jringo303@gmail.com>
2026-05-10 07:04:21 -0400

    updates

--- src/components/home/science-should-be.tsx
+++ src/components/home/science-should-be.tsx
@@ -1,17 +1,26 @@
+const PRINCIPLES: Array<{ num: string; title: string }> = [
+  { num: "01", title: "Community-owned" },
+  { num: "02", title: "Collaborative" },
+  { num: "03", title: "Auditable" },
+];
+
 export function ScienceShouldBe() {
   return (
     <section
-      className="ssb"
+      className="ssb ssb-rule"
       id="values"
       data-screen-label="Science Should Be"
     >
       <div className="wrap-narrow">
         <h2 className="ssb-h">Science should be</h2>
-        <ul className="ssb-list">
-          <li>Community-owned</li>
-          <li>Collaborative</li>
-          <li>Auditable</li>
-        </ul>
+        <div className="ssb-rule-line">
+          {PRINCIPLES.map((p, i) => (
+            <span key={p.num} className="ssb-rule-item">
+              {i > 0 && <span className="rule" aria-hidden="true" />}
+              <span className="value">{p.title}</span>
+            </span>
+          ))}
+        </div>
       </div>
     </section>
   );
